b6f2c28582403aa006be03c9cc8d02f73a473667
Nida Cambay <cambayn@gmail.com>
2025-06-25 15:55:40 +0300

    edit userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+# Sistem paketlerini güncelle
 sudo apt update -y
 sudo apt install -y docker.io
+
+# Docker servisini başlat ve otomatik başlatmayı etkinleştir
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Docker Hub'dan image çek ve yayına al
-docker run -d -p 80:80 nidacambay/treepedia-site
+# Daha önce aynı container varsa durdur ve sil
+sudo docker rm -f treepedia-container || true
+
+# En güncel imajı çek ve yayına al
+sudo docker pull nidacambay/treepedia-site
+sudo docker run -d --name treepedia-container -p 80:80 nidacambay/treepedia-site
